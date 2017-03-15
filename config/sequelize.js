20632cb545fe085d3b537d2e776ca5f090265fb7
mklishevych <mklishevych@gmail.com>
2017-03-15 21:50:17 +0200

    config

---
+++ config/sequelize.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// var config = require('./config');
+// var Sequelize = require('sequelize');
+var models = require('../models/index');
+
+// var sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, {
+//     host: config.db.host,
+//     port: config.db.port,
+//     dialect: 'postgres'
+// });
+
+module.exports = models;
