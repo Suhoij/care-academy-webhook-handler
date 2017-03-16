2bbd5f45a989dc3e2547b952fcc10218a8ac73ba
mklishevych <mklishevych@gmail.com>
2017-03-17 00:36:58 +0200

    added log rotation

--- config/winston.js
+++ config/winston.js
@@ -1,6 +1,17 @@
 'use strict';
 
-var winston = require('winston');
-winston.add(winston.transports.File, { filename: 'app.log' });
+const winston = require('winston');
+const Rotate = require('winston-logrotate').Rotate;
+const path = require('path');
+
+winston.add(Rotate, {
+    file: path.resolve('app.log'), // this path needs to be absolute
+    colorize: false,
+    timestamp: true,
+    json: false,
+    max: '100m',
+    keep: 5,
+    compress: true
+});
 
 module.exports = winston;
