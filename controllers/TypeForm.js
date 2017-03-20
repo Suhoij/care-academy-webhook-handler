49acbc6faaf906703a5fa7d8223e7c2e50e5fa97
mklishevych <mklishevych@gmail.com>
2017-03-20 21:33:30 +0200

    implemented mail chimp integration

--- controllers/TypeForm.js
+++ controllers/TypeForm.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const TypeFormResponseDTO = require('../dto/TypeFormResponse');
+const MailChimp = require('../services/MailChimp');
+
 function TypeFormController(db, winston) {
     this.db = db;
     this.winston = winston;
@@ -9,12 +12,25 @@ function TypeFormController(db, winston) {
 TypeFormController.prototype.post = function(request, reply) {
     this.winston.info('Received a web hook from TypeForm.');
 
-    var model = this.db.WebHookJson.build();
+    const model = this.db.WebHookJson.build();
     model.origin = this.db.WebHookJson.Origins.TYPE_FORM;
     model.data = request.payload;
 
     model
         .save()
+        .then(function () {
+            const dto = new TypeFormResponseDTO(request.payload);
+
+            if (dto.getHidden('email')) {
+                MailChimp.updateCareGiverLevel(
+                    dto.getHidden('email'),
+                    dto.getCalculatedScore(),
+                    this.winston
+                );
+            } else {
+                this.winston.error('Submitted TypeForm "%s" with event ID "%s" is missing email.', dto.getFormId(), dto.getEventId());
+            }
+        }.bind(this))
         .catch(function(err) {
             this.winston.error(err);
         }.bind(this));
