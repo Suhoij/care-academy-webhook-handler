26c8a903bd8a6e15b3985519bbcfa442c8a1b4a9
dmitriy.sushko <suhoij@gmail.com>
2017-03-20 12:40:33 -0400

    1. Change the config file to run the migrations localy and in prod. 2. Change the port the app is running on. By default the nging reverse proxy use that.

--- models/index.js
+++ models/index.js
@@ -5,14 +5,20 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
+var dbConfig  = require('../config/database.js')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+
+var sequelize = new Sequelize(
+		dbConfig.database,
+		dbConfig.username,
+		dbConfig.password,
+		{
+			"dialect"   : dbConfig.dialect,
+			"host"      : dbConfig.host
+		}
+    );
+
 
 fs
   .readdirSync(__dirname)
